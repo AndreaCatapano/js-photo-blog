7bd1ee61630ba55eb84ebb08a5dec70ed197b00d
AndreaCatapano <a.catapano96@gmail.com>
2025-03-14 15:14:39 +0100

    feat: called api with axios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("ciao");
+/* 
+            <div class="card">
+                <img src="https://marcolanci.it/boolean/assets/pictures/1.png" alt="">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <img class="pin" src="img/pin.svg" alt="">
+            </div>
+*/
+
+const urlApi = "https://lanciweb.github.io/demo/api/pictures/";
+
+axios.get(urlApi).then(response => {
+    console.log(response.data)
+}).catch(error => console.error(error));
+
+
