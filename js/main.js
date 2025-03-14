6d5f477f02dcea6e8fa92f08532acd1e8579fee0
AndreaCatapano <a.catapano96@gmail.com>
2025-03-14 16:08:40 +0100

    doc: added a report for day1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-/* 
-            <div class="card">
-                <img src="https://marcolanci.it/boolean/assets/pictures/1.png" alt="">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-                <img class="pin" src="img/pin.svg" alt="">
-            </div>
-*/
-
 const urlApi = "https://lanciweb.github.io/demo/api/pictures/";
 const parentElement = document.getElementById("card-container")
 
