aad92aca494b84470b820e327277bd4434578be1
AndreaCatapano <a.catapano96@gmail.com>
2025-03-14 16:01:32 +0100

    feat: added breakpoints to css (desktop first)

--- js/main.js
+++ js/main.js
@@ -9,11 +9,15 @@
 const urlApi = "https://lanciweb.github.io/demo/api/pictures/";
 const parentElement = document.getElementById("card-container")
 
-axios.get(urlApi).then(response => {
-    renderHTML(parentElement, response.data);
-}).catch(error => console.error(error));
 
+// Chiamata all'endpoint urlAPI alla risposta invoca la funzione renderHTML
+axios.get(urlApi).then(response => renderHTML(parentElement, response.data)).catch(error => console.error(error));
 
+/*
+Funzione che accetta due parametri:
+- un elemento "padre" che è il contenitore in cui appendere i nuovi elementi.
+- un array su cui itineremo per generare un nuovo elemento con createHTML per ogni elemento dell'array.
+*/ 
 function renderHTML(parent, childArray) {
     parent.innerHTML = "";
     for (let i = 0; i < childArray.length; i++) {
@@ -23,6 +27,10 @@ function renderHTML(parent, childArray) {
     }
 }
 
+
+/*
+Funzione che accetta un parametro dalla quale utilizzeremo le proprietà del oggetto per creare un nuove elemento HTML
+*/ 
 function createHTML(member) {
     let element = document.createElement("div");
     element.classList.add("card");
